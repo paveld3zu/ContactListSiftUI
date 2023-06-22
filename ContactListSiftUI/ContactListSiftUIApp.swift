9134794673910e3ba8c2e9efbe4cb4635e4ada05
paveld3zu <pavel.karunnyj@yandex.ru>
2023-06-22 18:03:30 +0300

    Initial Commit

---
+++ ContactListSiftUI/ContactListSiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSiftUIApp.swift
+//  ContactListSiftUI
+//
+//  Created by Pavel Karunnyj on 22.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
